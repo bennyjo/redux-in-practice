a619f75c5a9da254debb29f5a66b9a414795a963
Benny Johansson <benny.json@gmail.com>
2017-12-17 18:28:48 +0000

    feat(reducers): mock some books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'The Fellowship Of The Ring' },
+    { title: 'The Fire Child' },
+    { title: 'The Chimp Paradox' }
+  ]
+}
