0af6aca72c140c84f7c4bd385d0ea4d02ea0e497
Benny Johansson <benny.json@gmail.com>
2018-01-02 20:21:58 +0100

    feat(book-detail): title and pages

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'The Fellowship Of The Ring' },
-    { title: 'The Fire Child' },
-    { title: 'The Chimp Paradox' }
+    { title: 'Javascript: The Good Parts', pages: 14 },
+    { title: 'The Fellowship Of The Ring', pages: 46 },
+    { title: 'The Fire Child', pages: 34 },
+    { title: 'The Chimp Paradox', pages: 1 }
   ]
 }
